9bea67758ec080cba464425b52d7a1b51e0c6d72
Nabellass <104151750+nabellass@users.noreply.github.com>
2022-08-06 14:33:34 +0700

    Update fortuneteller.js

--- fortuneteller.js
+++ fortuneteller.js
@@ -4,7 +4,7 @@ const result = document.getElementById('result');
 const card = ['Wonderfull!! This is Gonna be Your Day!!', 'Try Again Tomorrow and Good Luck', 'It Is The Worst. Be Strong.'];
 
 function getRandom() {
-  const array = [0,0,0,1,1,1,2,2,2];
+  const array = [0,1,1,2,2,2];
   const randomArray = array[Math.floor(Math.random()* array.length)];
   
   return randomArray;
